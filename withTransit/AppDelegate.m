92d0c9b46346cfa18cf23ad6dffed273cc4b13e3
Tamiiy <rashifamido@gmail.com>
2015-01-07 07:48:57 +0800

    parse導入完了(動作確認テストまで)

--- withTransit/AppDelegate.m
+++ withTransit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,18 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"EqW6HENjIqZ1zZxHgjW93Z17tLXGNKseXj88h1L0"
+                  clientKey:@"4sW9zW3hygG1AWa8BJGlDFGwnFLN1vs0pyJa3nBW"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
     // Override point for customization after application launch.
     return YES;
 }
